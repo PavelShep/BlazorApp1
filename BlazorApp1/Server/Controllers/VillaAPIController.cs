a43778262442611e62beeaaea1f328fdc79d8efb
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2024-05-27 13:02:50 +0200

    Add project files.

---
+++ BlazorApp1/Server/Controllers/VillaAPIController.cs
@@ -0,0 +1,27 @@
+﻿using BlazorApp1.Server.Data;
+using BlazorApp1.Shared.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        //database
+        private readonly ApplicationDbContext _db;
+
+        public VillaAPIController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(_db.Villas.ToList());
+        }
+    }
+}
